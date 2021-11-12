e9ae8e1f12f2fb13c02ef12749a769eccd897cfb
Rimvydas Vainutis <rimvydas.vainutis@gmail.com>
2021-11-12 20:15:01 +0200

    New lesson, DropDown

---
+++ automatinisTestavimasPamokos/Page/DropDownPage.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automatinisTestavimasPamokos.Page
+{
+    public class DropDownPage : BasePage
+    {
+        private const string PageAddress = "https://demo.seleniumeasy.com/basic-select-dropdown-demo.html";
+        private const string ResultText = "Day selected :- ";
+        private IWebElement ResultTextElement => Driver.FindElement(By.CssSelector(".selected-value"));
+        private SelectElement DropDown => new SelectElement(Driver.FindElement(By.Id("select-demo")));
+        public DropDownPage(IWebDriver webDriver) : base(webDriver)
+        {
+            Driver.Url = PageAddress;
+        }
+
+        public DropDownPage SelectFromDropdownByText(string text)
+        {
+            DropDown.SelectByText(text);
+            return this;
+        }
+
+        public DropDownPage SelectFromDropdownByValue(string text)
+        {
+            DropDown.SelectByValue(text);
+            return this;
+        }
+
+        public DropDownPage VerifyResult(string selectedDay)
+        {
+            Assert.IsTrue(ResultTextElement.Text.Equals(ResultText + selectedDay), $"Result is wrong, not {selectedDay}");
+            return this;
+        }
+    }
+}
---
+++ automatinisTestavimasPamokos/Test/DropDownTest.cs
@@ -0,0 +1,36 @@
+﻿using automatinisTestavimasPamokos.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automatinisTestavimasPamokos.Test
+{
+    public class DropDownTest
+    {
+        private static DropDownPage _page;
+
+        [OneTimeSetup]
+        public static void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            _page = new DropDownPage(driver);
+        }
+        
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            _page.CloseBrowser();
+        }
+
+        //[Order(1)]
+        [Test]
+        public 
+    }
+}
