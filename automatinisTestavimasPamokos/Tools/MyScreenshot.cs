541497293a48fa3b4d15815e98e9e11d45f88523
Rimvydas Vainutis <rimvydas.vainutis@gmail.com>
2021-11-17 19:59:02 +0200

    202111171959

--- automatinisTestavimasPamokos/Tools/MyScreenshot.cs
+++ automatinisTestavimasPamokos/Tools/MyScreenshot.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.Extensions;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,9 @@ namespace automatinisTestavimasPamokos.Tools
             string screenshotDirectory = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
             string screenshotFolder = Path.Combine(screenshotDirectory, "screenshot");
             Directory.CreateDirectory(screenshotFolder);
+            string screenshotName = $"{TestContext.CurrentContext.Test.Name}_{DateTime.Now:HH - mm - ss}.png";
+            string screenshotPath = Path.Combine(screenshotFolder, screenshotName);
+            myBrowserScreenshot.SaveAsFile(screenshotName, ScreenshotImageFormat.Png);
         }
     }
 }
