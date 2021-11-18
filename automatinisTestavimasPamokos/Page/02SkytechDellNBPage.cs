54ce78b91a0afba166fb03bef87c764e0149bb0d
Rimvydas Vainutis <rimvydas.vainutis@gmail.com>
2021-11-18 15:37:27 +0200

    202111181537

--- automatinisTestavimasPamokos/Page/02SkytechDellNBPage.cs
+++ automatinisTestavimasPamokos/Page/02SkytechDellNBPage.cs
@@ -17,15 +17,11 @@ namespace automatinisTestavimasPamokos.Page
         private const string SkytechDellNotebooksAddress = "https://www.skytech.lt/nesiojami-kompiuteriai-nesiojami-kompiuteriai-c-86_165_81.html?f=s(),g(142),p(),k(317.89,5668.00)&frag=&fragd=0&pav=undefined&sort=5a&sand=1&grp=1&pagesize=100&page=1";
 
         // web elementai
-        //private IWebElement NotebookDellCategoryButton => Driver.FindElement(By.CssSelector("#f-g-142 > div.line-wrap > span"));
-        //private IWebElement NotebookDellSecondPageButton => Driver.FindElement(By.CssSelector("#centerpanel > div.contentbox-center-wrap.nopad > table.pagebar > tbody > tr > td.pagenav > a:nth-child(2) > div > div.page"));
-        //private IWebElement NotebookDellThrirdPageButton => Driver.FindElement(By.CssSelector("#centerpanel > div.contentbox-center-wrap.nopad > table.pagebar > tbody > tr > td.pagenav > a:nth-child(3) > div > div.page"));
+        
         private IWebElement NotebookDellFirstAddToCartButton => Driver.FindElement(By.CssSelector("#centerpanel > div.contentbox-center-wrap.nopad > table.productListing > tbody > tr:nth-child(5) > td:nth-child(6) > div > div.button-label > input"));
         private IWebElement NotebookDellSecondAddToCartButton => Driver.FindElement(By.CssSelector("#centerpanel > div.contentbox-center-wrap.nopad > table.productListing > tbody > tr:nth-child(6) > td:nth-child(6) > div > div.button-label > input"));
         private IWebElement CartButton => Driver.FindElement(By.CssSelector("#krepselis > div > a > span"));
-        
-        //        
-
+                
         public SkytechDellNotebooksPage(IWebDriver webDriver) : base(webDriver)
         {
             Driver.Url = SkytechDellNotebooksAddress;
@@ -35,31 +31,13 @@ namespace automatinisTestavimasPamokos.Page
         {
             Thread.Sleep(500);
             return this;
-        }
-
-        //public SkytechDellNotebooksPage ClickNotebookDellCategoryButton()
-        //{
-        //    NotebookDellCategoryButton.Click();
-        //    return this;
-        //}
-
-        //public SkytechDellNotebooksPage ClickNotebookDellSecondPageButton()
-        //{
-        //    NotebookDellSecondPageButton.Click();
-        //    return this;
-        //}
+        }        
 
         public SkytechDellNotebooksPage ClickNotebookDellFirstAddToCartButton()
         {
             NotebookDellFirstAddToCartButton.Click();
             return this;
-        }
-
-        //public SkytechDellNotebooksPage ClickNotebookDellThrirdPageButton()
-        //{
-        //    NotebookDellThrirdPageButton.Click();
-        //    return this;
-        //}
+        }        
 
         public SkytechDellNotebooksPage ClickNotebookDellSecondAddToCartButton()
         {
