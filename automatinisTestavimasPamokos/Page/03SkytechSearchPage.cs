a9e9dfae5605bd73ed905218d5bfa973ad9fe5db
Rimvydas Vainutis <rimvydas.vainutis@gmail.com>
2021-11-19 13:32:24 +0200

    202111191332

--- automatinisTestavimasPamokos/Page/03SkytechSearchPage.cs
+++ automatinisTestavimasPamokos/Page/03SkytechSearchPage.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,15 @@ namespace automatinisTestavimasPamokos.Page
     {
         // konstantos
         private const string SkytechCartAddress = "https://www.skytech.lt/search_form.html";
+        private const string EmptySearchResult = "Nėra prekių, atitinkančių užduotus paieškos kriterijus.";
 
         // web elementai
-        
+        private IWebElement SearchByAllWordsField => Driver.FindElement(By.CssSelector("#searchform > table > tbody > tr:nth-child(1) > td:nth-child(3) > input[type=text]"));        
+        private IWebElement TitlesAndDescriptionsRadioButton => Driver.FindElement(By.CssSelector("#searchform > table > tbody > tr:nth-child(4) > td:nth-child(3) > input:nth-child(3)"));
+        private SelectElement CategoryList => new SelectElement(Driver.FindElement(By.CssSelector("#searchform > table > tbody > tr:nth-child(5) > td:nth-child(3) > select")));
+        private IWebElement ManufacturerField => Driver.FindElement(By.Id("gamintojas"));
+        private IWebElement SiteSearchResultEmpty => Driver.FindElement(By.CssSelector("#centerpanel > div.contentbox-center-wrap.nopad > table > tbody > tr > td"));
+        private IWebElement SearchButton => Driver.FindElement(By.CssSelector("#searchform > table > tbody > tr:nth-child(8) > td:nth-child(1) > input"));
 
         public SkytechSearchPage(IWebDriver webDriver) : base(webDriver)
         {
@@ -27,5 +34,58 @@ namespace automatinisTestavimasPamokos.Page
             Thread.Sleep(500);
             return this;
         }
+
+        public SkytechSearchPage ClickSearchByAllWordsField()
+        {
+            SearchByAllWordsField.Click();
+            return this;
+        }
+
+        public SkytechSearchPage SendSearchTextToSearchByAllWordsField(string searchQuery)
+        {
+            SearchByAllWordsField.Clear();
+            SearchByAllWordsField.SendKeys(searchQuery);
+            return this;
+        }
+
+        public SkytechSearchPage ClickTitlesAndDescriptionsRadioButton()
+        {
+            TitlesAndDescriptionsRadioButton.Click();
+            return this;
+        }
+
+        public SkytechSearchPage ClickManufacturerField()
+        {
+            ManufacturerField.Click();
+            return this;
+        }
+        public SkytechSearchPage SendSearchTextManufacturerField(string manufacturer)
+        {
+            ManufacturerField.Clear();
+            ManufacturerField.SendKeys(manufacturer);
+            return this;
+        }
+
+        public SkytechSearchPage CheckSiteSearchResultEmpty()
+        {
+            Assert.AreEqual(EmptySearchResult, SiteSearchResultEmpty.Text, "Prekiu buvo rasta.");
+            return this;
+        }
+
+        public SkytechSearchPage SelectFromCategoryList(string value)
+        {
+            CategoryList.SelectByValue(value);
+            return this;
+        }
+
+        public SkytechSearchPage ClickSearchButton()
+        {
+            SearchButton.Click();
+            return this;
+        }
+
+
+
+
     }
 }
--- automatinisTestavimasPamokos/Test/03SkytechSearchTest.cs
+++ automatinisTestavimasPamokos/Test/03SkytechSearchTest.cs
@@ -29,6 +29,26 @@ namespace automatinisTestavimasPamokos.Test
             _page.CloseBrowser();
         }
 
-
+        // 270 - Kategorija: periferija, biuro iranga
+        [TestCase("AW27255981D", "270", "Dell", TestName = "05 Test search fields, get empty list.")]
+        public void TestItemCount(string searchQuery, string categoryValue, string manufacturer)
+        {
+            _page.ClickSearchByAllWordsField()
+                .ThreadSleep500()
+                .SendSearchTextToSearchByAllWordsField(searchQuery)
+                .ThreadSleep500()
+                .ClickTitlesAndDescriptionsRadioButton()
+                .ThreadSleep500()
+                .SelectFromCategoryList(categoryValue)
+                .ThreadSleep500()
+                .ClickManufacturerField()
+                .ThreadSleep500()
+                .SendSearchTextManufacturerField(manufacturer)
+                .ThreadSleep500()
+                .ClickSearchButton()
+                .ThreadSleep500()
+                .CheckSiteSearchResultEmpty()
+                .ThreadSleep500();            
+        }
     }
 }
